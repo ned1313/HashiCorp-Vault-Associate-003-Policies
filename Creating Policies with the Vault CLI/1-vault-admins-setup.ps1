877d3a7f49f8e3b7b315806a70bd473c4b0154ef
ned1313 <ned.bellavance@gmail.com>
2025-02-17 11:04:58 -0500

    remove commands

--- Creating Policies with the Vault CLI/1-vault-admins-setup.ps1
+++ Creating Policies with the Vault CLI/1-vault-admins-setup.ps1
@@ -6,9 +6,3 @@ $env:VAULT_ADDR = "http://127.0.0.1:8200"
 
 # Create the auth_admins policy
 vault policy write vault-admins vault-admins.hcl
-
-# Create a token with the auth_admins policy
-vault token create -policy=auth_admins
-
-# Login with the token
-vault login
